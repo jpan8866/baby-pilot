78b90a691d9944f9de802f7f0b43843f1ef2b5b6
jo6109072 <john.pan@mail.mcgill.ca>
2024-01-20 14:56:29 -0500

    add compute turn logic

--- autopilot/Lab 1 Part A/improved_roomba.py
+++ autopilot/Lab 1 Part A/improved_roomba.py
@@ -15,7 +15,7 @@ def gear(is_drive: bool):
 def compute_turn_direction(scan_list: list):
     # figure out what direction to turn (90 deg)
     # if either side is not 2, turn right
-    if scan_list[:3] == [2,2,2] or scan_list[8:] == [2,2,2]:
+    if scan_list[:3] == [2,2,2] and scan_list[8:] == [2,2,2]:
         fc.turn_right(speed)
     elif scan_list[:3] != [2,2,2]:
         print("turning right")
