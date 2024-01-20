a7c10064239ca5d3c9a02e1477c4cc6f126af507
jo6109072 <john.pan@bell.ca>
2024-01-20 15:48:49 -0500

    add compute turn logic

--- autopilot/Lab 1 Part A/improved_roomba.py
+++ autopilot/Lab 1 Part A/improved_roomba.py
@@ -5,6 +5,8 @@ import picar_4wd as fc
 import time
 
 speed = 30
+turn_duration = 4  # calculated by taking car_length / speed
+
 
 def gear(is_drive: bool):
     if is_drive:
@@ -12,20 +14,44 @@ def gear(is_drive: bool):
     else:
         fc.stop()
 
-def compute_turn_direction(scan_list: list):
+
+def compute_turn_direction(scan_list: list) -> str:
     # figure out what direction to turn (90 deg)
     # if either side is not 2, turn right
     if scan_list[:3] == [2,2,2] and scan_list[8:] == [2,2,2]:
         fc.turn_right(speed)
-    elif scan_list[:3] != [2,2,2]:
+        return "right"
+    elif scan_list[8:] == [2, 2, 2]:
         print("turning right")
-        # fc.turn_right(speed)
-    else:
+        fc.turn_right(speed)
+        return "right"
+    elif scan_list[:3] == [2, 2,2]:
         print("turning left")
-        # fc.turn_left(speed)
+        fc.turn_left(speed)
+        return "left"
+    else: # blocked in
+        print("boxed in")
+        fc.backward(speed)
+        time.sleep(turn_duration)
+        fc.turn_right()
+        return "right"
 
+def straighten_left(scan_list):
+    while any(s == 2 for s in scan_list[:3]):
+        fc.turn_left()
+        # Remember to recheck the scan_list after each turn
+        scan_list = fc.scan_step(35)
+
+def straighten_right():
+    # turn right
+    while any(s == 2 for s in scan_list[8:]):
+        fc.turn_right()
+        # Remember to recheck the scan_list after each turn
+        scan_list = fc.scan_step(35)
 
 def main():
+    turn_start_time = 0
+    direction = "forward"
     while True:
         scan_list = fc.scan_step(35)
 
@@ -36,15 +62,29 @@ def main():
         print(tmp)
 
         if tmp != [2, 2, 2, 2]: # directly in front of car
-            print("obstacle")
-            gear(is_drive=False)  # Stop if obstacle detected
-            compute_turn_direction(scan_list)
-            # logic to check whether to turn right or left
-            # straight until does not sense anything on either side + 2 more second to let whole body get accross
-            # turn back same amount of seconds
-            # turn back straight (optional)
+            if direction == "forward":
+                print("obstacle")
+                gear(is_drive=False)  # Stop if obstacle detected
+                direction = compute_turn_direction(scan_list)
+                turn_start_time = time.time()
+                # logic to check whether to turn right or left
+                # straight until does not sense anything on either side + 2 more second to let whole body get accross
+                # turn back same amount of seconds
+                # turn back straight (optional)
+            # else keep turning
         else:
-            gear(is_drive=True)  # Start driving
+            gear(is_drive=True) # move forward in current direction
+            if direction != "forward":
+                # measure time since last loop here
+                elapsed_time = time.time() - turn_start_time
+                if elapsed_time >= turn_duration:
+                    print("turning back to original heading")
+                    if direction == "right":
+                        straighten_left()
+                        direction = "forward"
+                    elif direction == "left":
+                        straighten_right()
+                        direction = "forward"
 
 if __name__ == "__main__":
     try:
