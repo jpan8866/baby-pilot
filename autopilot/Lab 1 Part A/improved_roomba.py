06bacf3c2c2255734d9e5902949a792b2b0fd8a1
jo6109072 <john.pan@bell.ca>
2024-01-20 16:38:12 -0500

    add boxed in logic

--- autopilot/Lab 1 Part A/improved_roomba.py
+++ autopilot/Lab 1 Part A/improved_roomba.py
@@ -18,14 +18,14 @@ def gear(is_drive: bool):
 def compute_turn_direction(scan_list: list) -> str:
     # figure out what direction to turn (90 deg)
     # if either side is not 2, turn right
-    if scan_list[:3] == [2,2,2] and scan_list[7:] == [2,2,2]:
+    if scan_list[:2] == [2,2] and scan_list[8:] == [2,2]:
         fc.turn_right(speed)
         return "right"
-    elif scan_list[7:] == [2, 2, 2]:
+    elif scan_list[8:] == [2, 2]:
         print("turning right")
         fc.turn_right(speed)
         return "right"
-    elif scan_list[:3] == [2, 2, 2]:
+    elif scan_list[:2] == [2, 2]:
         print("turning left")
         fc.turn_left(speed)
         return "left"
@@ -68,7 +68,7 @@ def main():
         if not scan_list or len(scan_list) != 10:
             continue
 
-        tmp = scan_list[3:7]
+        tmp = scan_list[4:6]
         print("tmp", scan_list, tmp)
 
         if tmp != [2, 2, 2, 2]: # directly in front of car
