870d188811132d0b47f41de53eafe212764408ea
jo6109072 <john.pan@bell.ca>
2024-01-20 22:55:09 -0500

    fix starting angle

--- autopilot/Lab 1 Part A/improved_roomba_cleaned.py
+++ autopilot/Lab 1 Part A/improved_roomba_cleaned.py
@@ -40,7 +40,10 @@ def get_scan_list():
 
 def main():
     while True:
-        scan_list = get_scan_list()
+        scan_list = fc.scan_step(scan_ref)
+        # waits for a full sweep before driving off
+        if not scan_list or len(scan_list) != 10:
+            continue
 
         print(scan_list)
         tmp = scan_list[3:7]
