39fb027c6e5a1529e3f38388865a88f99951ee87
jo6109072 <john.pan@mail.mcgill.ca>
2024-02-01 00:21:24 -0500

    cleanups

--- autopilot/Lab_1_B/routing_test.py
+++ autopilot/Lab_1_B/routing_test.py
@@ -1,6 +1,6 @@
 from path_finder import a_star_search_4dir, Node
 from advanced_mapping import scan_environment
-from utils import visualize_path, visualize_map
+from utils import visualize_path
 
 grid = scan_environment()
 # Create a 50x50 grid where 0 represents empty cells
@@ -13,10 +13,3 @@ goal = Node(GRID_SIZE//2, GRID_SIZE-1)
 # Run A* search to find the path
 path = a_star_search_4dir(grid, start, goal)
 visualize_path(grid, path)
-
-# if path:
-#     print("Path found:")
-#     for x, y in path:
-#         print(f"({x}, {y})")
-# else:
-#     print("No path found.")
