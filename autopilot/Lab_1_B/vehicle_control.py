c1a621fbf6c140fd7388102022a444d78de7cac1
jo6109072 <john.pan@mail.mcgill.ca>
2024-02-01 22:54:47 -0500

    test angle

--- autopilot/Lab_1_B/vehicle_control.py
+++ autopilot/Lab_1_B/vehicle_control.py
@@ -20,20 +20,21 @@ def drive(distance: int, power: int = 10):
     fc.right_rear_speed.deinit()
 
 
-def turn_diagonal(angle: int, power: int = 10):
-    '''
-    stop car first before initiating turn
-    experiment with gradually scaling up acceleration
-    experiment with slower speed
-    '''
-    fc.stop()
-    time.sleep(0.1)
-    fc.turn_left(2)
-    time.sleep(0.5)
-    fc.turn_left(4)
-    time.sleep(0.5)
-    fc.turn_left(6)
-    fc.stop()
+# def turn_diagonal(angle: int):
+#     '''
+#     stop car first before initiating turn
+#     experiment with gradually scaling up acceleration
+#     experiment with slower speed
+#     '''
+#     fc.stop()
+#     time.sleep(0.1)
+#     fc.turn_left(2)
+#     time.sleep(0.5)
+#     fc.turn_left(4)
+#     time.sleep(0.5)
+#     fc.turn_left(6)
+#     fc.stop()
+
 
 def turn(angle: int, power: int = 1):
     fc.stop()
@@ -49,6 +50,7 @@ def turn(angle: int, power: int = 1):
         a += 180/math.pi * 2 * speed * 0.05 / 17
     fc.stop()
     s.deinit()
+
 
 if __name__ == "__main__":
     # drive(15.75)  # length of an iphone xs max for testing
