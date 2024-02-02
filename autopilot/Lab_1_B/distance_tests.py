316591460ab8f61c30572588b8d0f62d9f30149a
jo6109072 <john.pan@mail.mcgill.ca>
2024-02-01 20:54:29 -0500

    add test for speed

--- autopilot/Lab_1_B/distance_tests.py
+++ autopilot/Lab_1_B/distance_tests.py
@@ -21,11 +21,13 @@ def drive(distance: int, power: int=10):
     fc.start_speed_thread()
     x = 0
     fc.forward(power)
-    while x < distance:
+    while x < distance*0.95:
         time.sleep(0.1)
         s = fc.speed_val()
         x += s * 0.1
         print("%scm" % x)
+    fc.left_rear_speed.deinit()
+    fc.right_rear_speed.denit()
     fc.stop()
 
 
