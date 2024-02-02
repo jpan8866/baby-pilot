df63fbcaa94feba3eee3e9611c46e5a0eca68d72
jo6109072 <john.pan@mail.mcgill.ca>
2024-02-01 19:52:46 -0500

    add test for speed

--- autopilot/Lab_1_B/distance_tests.py
+++ autopilot/Lab_1_B/distance_tests.py
@@ -1,3 +1,19 @@
-from picar_4wd.speed import test3
+from picar_4wd.speed import *
+import picar_4wd as fc
 
-test3()
+def test():
+    speed = Speed(25)
+    speed.start()
+    fc.forward(100)
+    x = 0
+    for i in range(20):
+        time.sleep(0.1)
+        speed = speed()
+        x += speed * 0.1
+        print("%smm/s" % speed)
+    print("%smm" % x)
+    speed.deinit()
+    fc.stop()
+
+if __name__ == "__main__":
+    test()
