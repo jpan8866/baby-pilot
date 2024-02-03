c6d17458d03ee8287d96e3abf1346b908cdf4bca
jo6109072 <john.pan@mail.mcgill.ca>
2024-02-03 15:18:34 -0500

    generate file with grid data

--- autopilot/Lab_1_B/routing_test.py
+++ autopilot/Lab_1_B/routing_test.py
@@ -2,8 +2,11 @@ from path_finder import a_star_search_4dir, Node
 from advanced_mapping import scan_environment
 from utils import visualize_path
 import numpy as np
+import os
 
 grid = scan_environment()
+if not os.path.exists('/mnt/data'):
+    os.makedirs('/mnt/data')
 np.savetxt('/mnt/data/grid.txt', grid, fmt='%d') # scp file to laptop to view
 # Create a 50x50 grid where 0 represents empty cells
 GRID_SIZE = 50
