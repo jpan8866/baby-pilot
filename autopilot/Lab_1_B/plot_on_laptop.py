acd7ac7768b19d1b2ed34ad3605f36d4fd19b0e6
jo6109072 <john.pan@mail.mcgill.ca>
2024-02-03 15:52:46 -0500

    smaller angle increment

---
+++ autopilot/Lab_1_B/plot_on_laptop.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Replace 'path_to_grid.txt' with the path to the transferred file on your desktop
+grid = np.loadtxt('/Users/jo6109072/Desktop/grid.txt')
+grid = np.transpose(grid)
+plt.figure(figsize=(8, 8))
+plt.imshow(grid, cmap='gray', origin='lower')
+plt.colorbar()
+plt.show()
--- autopilot/Lab_1_B/settings.py
+++ autopilot/Lab_1_B/settings.py
@@ -1,3 +1,3 @@
 GRID_SIZE = 100
 PADDING_SIZE = 2  # number of cells to pad around a point in grid
-ANGLE_INCREMENT = 10
+ANGLE_INCREMENT = 5
