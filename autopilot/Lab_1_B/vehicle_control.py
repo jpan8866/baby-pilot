1844e34414f7784ae98c1fbd255e3c2035e1bf76
jo6109072 <john.pan@mail.mcgill.ca>
2024-02-05 13:38:51 -0500

    add drive_calculated

--- autopilot/Lab_1_B/vehicle_control.py
+++ autopilot/Lab_1_B/vehicle_control.py
@@ -73,7 +73,7 @@ def drive_calculated(distance: int) -> int:
     '''
     calculated 26cm/s for power = 1
     '''
-    speed = 26
+    speed = 25
     time_to_drive = distance/speed
     fc.forward(1)
     time.sleep(time_to_drive)
