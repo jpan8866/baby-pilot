e9c58f593b041cd1e2ebbd94d3d9195778711193
jo6109072 <john.pan@mail.mcgill.ca>
2024-02-04 20:00:28 -0500

    add continuous mapping with object detection

--- autopilot/Lab_1_B/self_driving.py
+++ autopilot/Lab_1_B/self_driving.py
@@ -194,7 +194,7 @@ def drive(distance: int, power: int = 10) -> int:
     '''
     x = 0
     fc.forward(power)
-    while x < distance * 0.5:
+    while x < distance * 0.6:
         if stop_event.is_set():
             fc.stop()
             traffic_cleared.wait()
