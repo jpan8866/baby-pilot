6aa203fa120452cb5184474e1498f8c4ae7e9334
jo6109072 <john.pan@mail.mcgill.ca>
2024-02-22 22:37:03 -0500

    feat: add flask app for video feed (test)

---
+++ autopilot/Lab_2/video_feed.py
@@ -0,0 +1,26 @@
+from flask import Flask, Response
+import cv2
+
+app = Flask(__name__)
+
+
+def generate_frames():
+    camera = cv2.VideoCapture(0)  # Use the appropriate camera index
+    if not camera.isOpened():
+        print("Cannot open camera")
+        exit()
+    while True:
+        success, frame = camera.read()
+        if not success:
+            break
+        else:
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
