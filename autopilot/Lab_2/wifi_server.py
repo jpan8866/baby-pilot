a0d31f75101009d1856f7a2e506ed9974977d92c
jo6109072 <john.pan@mail.mcgill.ca>
2024-02-22 20:49:54 -0500

    feat: add send metrics thread

--- autopilot/Lab_2/wifi_server.py
+++ autopilot/Lab_2/wifi_server.py
@@ -1,12 +1,39 @@
 import socket
 import picar_4wd as fc
 import threading
+import time
+import json
 
-HOST = "192.168.0.14" # IP address of your Raspberry PI
-PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
-POWER = 10            # Power of motors
+HOST = "192.168.0.14"  # IP address of your Raspberry PI
+PORT = 65432           # Port to listen on (non-privileged ports are > 1023)
+POWER = 10             # Power of motors
+
+
+def get_metrics():
+    metrics = {
+        'battery': fc.power_read(),
+        'speed': fc.speed_val(),
+        'cpu_temp': fc.cpu_temperature()
+    }
+    return metrics
+
 
 def handle_client(client, client_info):
+    # continuously send metrics e.g. battery, speed, cpu temperature
+    def send_data():
+        while True:
+            metrics = get_metrics()
+            data_message = json.dumps(metrics)
+            try:
+                client.sendall(data_message.encode('utf-8'))
+            except:
+                break  # exit loop if error
+            time.sleep(1)
+
+    # send data continuously in separate thread
+    data_thread = threading.Thread(target=send_data)
+    data_thread.start()
+
     try:
         while True:
             data = client.recv(1024)  # receive 1024 Bytes of message in binary format
@@ -34,16 +61,26 @@ def handle_client(client, client_info):
         print(f"Error handling client {client_info}: {e}")
     finally:
         client.close()
+        data_thread.join()  # stop data sending thread
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     print(f"Server listening on {HOST}:{PORT}")
-    while True:
-        client, client_info = s.accept()
-        print(f"Connection from {client_info}")
-        threading.Thread(target=handle_client, args=(client, client_info)).start()
+    try:
+        while True:
+            client, client_info = s.accept()
+            print(f"Connection from {client_info}")
+            threading.Thread(target=handle_client, args=(client, client_info)).start()
+    except KeyboardInterrupt:
+        # Handle Ctrl+C for a graceful shutdown
+        print("Server shutting down.")
+    except Exception as e:
+        # Handle specific exceptions based on your requirements
+        print(f"Exception: {e}")
+    finally:
+        s.close()
 
 
 ############## unused #################
